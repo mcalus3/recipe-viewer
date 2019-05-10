b26239b5f0ee921de8af1ea33a5dad8a31005f86
mcalus3 <marek-calus@wp.pl>
2019-05-11 00:25:31 +0200

    Rename Root to App.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import Root from 'components/Root'
+import App from 'components/App';
 
-render(<Root />, document.getElementById('application'))
+render(<App />, document.getElementById('application'));
