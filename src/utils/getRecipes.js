f9f94479fc6c6dd043d8b30c00dc62107080d785
mcalus3 <marek-calus@wp.pl>
2019-05-10 23:19:14 +0200

    Add getRecipes function.

---
+++ src/utils/getRecipes.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+// type Query = { ingredients: string[], page: number };
+// type Recipe = { title: String, href: String, ingredients: String[], thumbnailUrl: String };
+//function getRecipies(query: Query): Promise<Recipe[]>
+function getRecipes(query) {
+  return new Promise(async (resolve, reject) => {
+    const queryString = `i=` + query.ingredients.join() + `&&p=` + query.page;
+    try {
+      const response = await axios(
+        `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?` +
+          queryString,
+        {
+          timeout: 5
+        }
+      );
+      if (response.status === 200) {
+        const sortedRecipes = response.data.results.sort((a, b) =>
+          compareByTitle(a, b)
+        );
+        return resolve(sortedRecipes);
+      } else {
+        return reject('Request failed');
+      }
+    } catch (error) {
+      return reject('Timeout');
+    }
+  });
+}
+
+function compareByTitle(a, b) {
+  if (a.title < b.title) {
+    return -1;
+  }
+  if (a.title > b.title) {
+    return 1;
+  }
+  return 0;
+}
+
+export default getRecipes;
